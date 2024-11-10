88ece582990ae9d3eaa5739ed4151ec7ea80b14f
Kainoa Gardner <kainoagardner123@gmail.com>
2024-11-09 19:35:18 -0800

    finished

--- crack.c
+++ crack.c
@@ -4,45 +4,112 @@
 
 #include "md5.h"
 
-#if __has_include("fileutil.h")
 #include "fileutil.h"
-#endif
 
-#define PASS_LEN 50     // Maximum length any password will be.
-#define HASH_LEN 33     // Length of hash plus one for null.
+#define PASS_LEN 50 // Maximum length any password will be.
+#define HASH_LEN 33 // Length of hash plus one for null.
 
+// compare two strings alphabeticly
+int alphabeticSort(const void *a, const void *b) {
+  char **aa = (char **)a; // cast a to pointer to char pointer
+  char **bb = (char **)b; // cast b to pointer to char pointer
+  return strcmp(*aa, *bb);
+}
 
-int main(int argc, char *argv[])
-{
-    if (argc < 3) 
-    {
-        printf("Usage: %s hash_file dictionary_file\n", argv[0]);
-        exit(1);
+// create a list of passwords with 0-9 added to end of a base password
+char **getVariantPasswords(char *basePassword, int *size) {
+  int capacity = 15;
+  int passwordSize = strlen(basePassword);
+
+  // create array of passwords
+  char **arr = malloc(capacity * sizeof(char *));
+  *size = 0;
+
+  // add the original password
+  arr[*size] = malloc(strlen(basePassword) * sizeof(char));
+  strcpy(arr[*size], basePassword);
+  *size += 1;
+
+  // for int 0-9 add digit to end of orignal password and add it to arr
+  for (int i = 0; i < 10; i++) {
+    char temp[passwordSize + 2];
+    strcpy(temp, basePassword);
+    char addedDigit[2];
+    sprintf(addedDigit, "%d", i);
+    strcat(temp, addedDigit);
+
+    arr[*size] = malloc(strlen(temp) * sizeof(char));
+    strcpy(arr[*size], temp);
+
+    *size += 1;
+  }
+
+  // return password arr
+  return arr;
+}
+
+int main(int argc, char *argv[]) {
+  // check args
+  if (argc < 3) {
+    printf("Usage: %s hash_file dictionary_file\n", argv[0]);
+    exit(1);
+  }
+  printf("file loaded.\n");
+
+  // get array of strings from hash file
+  int size;
+  char **hashes = loadFileAA(argv[1], &size);
+
+  // sort array of strings alphabeticly
+  qsort(hashes, size, sizeof(char *), alphabeticSort);
+
+  // open dict file
+  FILE *dictFile = fopen(argv[2], "r");
+  if (!dictFile) {
+    fprintf(stderr, "Can't open %s for reading\n", argv[2]);
+    exit(1);
+  }
+
+  int cracked = 0;
+  char password[PASS_LEN];
+
+  // for each password in dict file check if its variants in hash array
+  while (fgets(password, PASS_LEN, dictFile) != NULL) {
+    // trim newline
+    char *nl = strchr(password, '\n');
+    if (nl)
+      *nl = '\0';
+
+    // get array of password variations
+    int variantSize;
+    char **variantPasswords = getVariantPasswords(password, &variantSize);
+
+    // for each password variation get hash and compare with hash array
+    for (int i = 0; i < variantSize; i++) {
+      // hash password
+      char *passwordHash = md5(variantPasswords[i], strlen(password));
+
+      // binary seach password in hash array
+      char **foundPassword =
+          bsearch(&passwordHash, hashes, size, sizeof(char *), alphabeticSort);
+
+      /* char *foundPassword = linearSearch(passwordHash, hashes, size); */
+
+      // if found print and incerment cracked
+      if (foundPassword) {
+        printf("%s %s\n", *foundPassword, variantPasswords[i]);
+        cracked++;
+      }
+      free(passwordHash);
     }
 
-    // TODO: Read the hashes file into an array.
-    //   Use either a 2D array or an array of arrays.
-    //   Use the loadFile function from fileutil.c
-    //   Uncomment the appropriate statement.
-    int size;
-    //char (*hashes)[HASH_LEN] = loadFile(argv[1], &size);
-    //char **hashes = loadFile(argv[1], &size);
-    
-    // CHALLENGE1: Sort the hashes using qsort.
-    
-    // TODO
-    // Open the password file for reading.
+    // free the variant password array of strings
+    freeAA(variantPasswords, variantSize);
+  }
 
-    // TODO
-    // For each password, hash it, then use the array search
-    // function from fileutil.h to find the hash.
-    // If you find it, display the password and the hash.
-    // Keep track of how many hashes were found.
-    // CHALLENGE1: Use binary search instead of linear search.
+  printf("%d hashes cracked!\n", cracked);
 
-    // TODO
-    // When done with the file:
-    //   Close the file
-    //   Display the number of hashes found.
-    //   Free up memory.
+  // close file and free hashe array
+  fclose(dictFile);
+  freeAA(hashes, size);
 }
---
+++ fileutil.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "fileutil.h"
+#include "md5.h"
+
+// Array of arrays
+char **loadFileAA(char *filename, int *size) {
+  // open file
+  FILE *in = fopen(filename, "r");
+  if (!in) {
+    perror("Can't open file");
+    exit(1);
+  }
+
+  // allocate initail array of arrays
+  int capacity = 100;
+  char **arr = malloc(capacity * sizeof(char *));
+  *size = 0;
+
+  int FILE_LINE_LENGTH = 500;
+  char line[FILE_LINE_LENGTH];
+
+  // check every line of file
+  while (fgets(line, FILE_LINE_LENGTH, in) != NULL) {
+    // if size of array same as capacity reallocate
+    if (*size == capacity) {
+      capacity += *size / 2;
+      arr = realloc(arr, capacity * sizeof(char *));
+    }
+
+    // trim endline
+    char *nl = strchr(line, '\n');
+    if (nl)
+      *nl = '\0';
+
+    // allocate memory for each line
+    int lineLength = strlen(line) + 1;
+    arr[*size] = malloc(lineLength * sizeof(char));
+
+    strcpy(arr[*size], line);
+
+    *size += 1;
+  }
+  // close file
+  fclose(in);
+  return arr;
+}
+
+// 2D Array
+char (*loadFile2D(char *filename, int *size))[COLS] {
+  // open file
+  FILE *in = fopen(filename, "r");
+  if (!in) {
+    perror("Can't open file");
+    exit(1);
+  }
+
+  // allocate initial array
+  int capacity = 100;
+  char(*arr)[COLS] = malloc(capacity * sizeof(char[COLS]));
+
+  *size = 0;
+
+  int FILE_LINE_LENGTH = 500;
+  char line[FILE_LINE_LENGTH];
+
+  // check every line in file
+  while (fgets(line, FILE_LINE_LENGTH, in) != NULL) {
+    // reallocate if size same as capacity
+    if (*size == capacity) {
+      capacity += *size / 2;
+      arr = realloc(arr, capacity * sizeof(char[COLS]));
+    }
+
+    // trim newline
+    char *nl = strchr(line, '\n');
+    if (nl)
+      *nl = '\0';
+
+    // copy line into array
+    strcpy(arr[*size], line);
+
+    *size += 1;
+  }
+
+  // close file
+  fclose(in);
+  return arr;
+}
+
+// seach each element in array of array for target
+char *substringSearchAA(char *target, char **lines, int size) {
+  for (int i = 0; i < size; i++) {
+    char *result;
+    result = strstr(lines[i], target);
+    if (result) {
+      return result;
+    }
+  }
+  return NULL;
+}
+
+// seach each element in 2d array for target
+char *substringSearch2D(char *target, char (*lines)[COLS], int size) {
+  for (int i = 0; i < size; i++) {
+    char *result;
+    result = strstr(lines[i], target);
+    if (result) {
+      return result;
+    }
+  }
+  return NULL;
+}
+
+// Free the memory used by array of arrays
+void freeAA(char **arr, int size) {
+  // free each element array
+  for (int i = 0; i < size; i++) {
+    free(arr[i]);
+  }
+
+  // free main array
+  free(arr);
+}
+
+// free memory used by 2d array
+void free2D(char (*arr)[COLS]) {
+  // only main array to free
+  free(arr);
+}
+
+char *linearSearch(char *passwordHash, char **hashes, int hashSize) {
+  // compare hash to line hash
+  for (int i = 0; i < hashSize; i++) {
+    if (strcmp(passwordHash, hashes[i]) == 0) {
+      return passwordHash;
+    }
+  }
+
+  return NULL;
+}
